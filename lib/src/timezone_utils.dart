1ffcedcacb6bb6dd2f231863a9b0537780ceaefa
Lam Thanh Nhan <hello@lamnhan.dev>
2025-11-26 22:41:46 +0700

    refactor: streamline Utc7 class by removing nullable location and simplifying timezone initialization

--- lib/src/timezone_utils.dart
+++ lib/src/timezone_utils.dart
@@ -6,13 +6,7 @@ import 'package:timezone/timezone.dart' as tz;
 /// that always use UTC+7 regardless of device timezone
 class Utc7 {
   static bool _timezoneInitialized = false;
-  static tz.Location? _utc7Location;
-
-  /// Initialize timezone to UTC+7 (Asia/Ho_Chi_Minh)
-  /// This should be called before using any timezone functions
-  static Future<void> initializeTimezone() async {
-    _initializeTimezoneSync();
-  }
+  static late tz.Location _utc7Location;
 
   /// Synchronously initialize timezone to UTC+7 (Asia/Ho_Chi_Minh)
   /// This is safe to call multiple times
@@ -28,14 +22,14 @@ class Utc7 {
   /// This always returns UTC+7 regardless of device timezone
   static tz.Location get location {
     _initializeTimezoneSync();
-    return _utc7Location!;
+    return _utc7Location;
   }
 
   /// Get the current TZDateTime in UTC+7 timezone
   /// This always returns UTC+7 time regardless of device timezone
   static tz.TZDateTime now() {
     _initializeTimezoneSync();
-    return tz.TZDateTime.now(_utc7Location!);
+    return tz.TZDateTime.now(_utc7Location);
   }
 
   /// Create a TZDateTime in UTC+7 timezone from individual components
@@ -52,7 +46,7 @@ class Utc7 {
   ]) {
     _initializeTimezoneSync();
     return tz.TZDateTime(
-      _utc7Location!,
+      _utc7Location,
       year,
       month,
       day,
@@ -71,18 +65,18 @@ class Utc7 {
     _initializeTimezoneSync();
     // Parse the string and convert to TZDateTime in UTC+7
     final parsed = DateTime.parse(formattedString);
-    return tz.TZDateTime.from(parsed, _utc7Location!);
+    return tz.TZDateTime.from(parsed, _utc7Location);
   }
 
-  /// Convert a TZDateTime to UTC+7 timezone as TZDateTime
-  /// If the TZDateTime is already in UTC+7, returns it as-is
-  /// This always uses UTC+7 regardless of device timezone
-  static tz.TZDateTime toUtc7(tz.TZDateTime dateTime) {
+  /// Converts any [DateTime] (local, UTC, or TZDateTime) to a [tz.TZDateTime]
+  /// in the UTC+7 timezone (Asia/Ho_Chi_Minh).
+  ///
+  /// If the input is already a [tz.TZDateTime] in UTC+7, it is returned unchanged.
+  ///
+  /// Regardless of the input's original timezone, the result is always in UTC+7.
+  static tz.TZDateTime toUtc7(DateTime dateTime) {
     _initializeTimezoneSync();
-    if (dateTime.location == _utc7Location) {
-      return dateTime;
-    }
-    return tz.TZDateTime.from(dateTime, _utc7Location!);
+    return tz.TZDateTime.from(dateTime, _utc7Location);
   }
 
   /// Create a TZDateTime from milliseconds since epoch in UTC+7 timezone
@@ -90,7 +84,7 @@ class Utc7 {
     _initializeTimezoneSync();
     final utc = DateTime.fromMillisecondsSinceEpoch(millisecondsSinceEpoch,
         isUtc: true);
-    return tz.TZDateTime.from(utc, _utc7Location!);
+    return tz.TZDateTime.from(utc, _utc7Location);
   }
 
   /// Get date only (00:00:00) as TZDateTime in UTC+7 timezone
@@ -98,7 +92,7 @@ class Utc7 {
     _initializeTimezoneSync();
     final tzDateTime = toUtc7(dateTime);
     return tz.TZDateTime(
-      _utc7Location!,
+      _utc7Location,
       tzDateTime.year,
       tzDateTime.month,
       tzDateTime.day,
